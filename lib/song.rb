4444181ef89eec46dc967055354e78f07a449339
Randy <randyisbusy@gmail.com>
2020-06-18 12:17:48 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class Song
-
+#I have been changed now for a pull request!
 
   def self.table_name
     self.to_s.downcase.pluralize
